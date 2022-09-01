0332bde7a5b9234d3fe639321cd9647c6f31c843
utkarsh-009 <utkarshpatil321@gmail.com>
2022-09-01 11:48:17 +0530

    Done

--- B_Mathematical_Circus.cpp
+++ B_Mathematical_Circus.cpp
@@ -6,6 +6,46 @@ void solve()
 {
     int n, k;
     cin >> n >> k;
+
+    if (k == 0)
+    {
+        cout << "NO" << endl;
+        return;
+    }
+
+    bool possible = true; // n/2 splits possible
+    for (int i = 1; i <= n; i += 2)
+    {
+        ll a = i, b = i + 1;
+        if (((a + k) * b) % 4 != 0 && ((b + k) * a) % 4 != 0)
+        {
+            possible = false;
+            break;
+        }
+    }
+
+    if (possible == false)
+    {
+        cout << "NO" << endl;
+    }
+
+    else
+    {
+        cout << "YES" << endl;
+        for (int i = 1; i <= n; i += 2)
+        {
+            ll a = i, b = i + 1;
+            if (((a + k) * b) % 4 == 0)
+            {
+
+                cout << i << " " << i + 1 << endl;
+            }
+            else
+            {
+                cout << i + 1 << " " << i << endl;
+            }
+        }
+    }
 }
 
 int main()
