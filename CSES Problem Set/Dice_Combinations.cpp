a05dbc2ac772807b0748003d83eda38d871ca571
utkarsh-009 <utkarshpatil321@gmail.com>
2022-09-21 23:50:56 +0530

    Done

--- CSES Problem Set/Dice_Combinations.cpp
+++ CSES Problem Set/Dice_Combinations.cpp
@@ -3,18 +3,22 @@
 #define mod 1000000007
 using namespace std;
 
+/*
+RECURSIVE:
+
 int cntCombs(int n)
 {
-
     // Base Case
+    // Combination not possible
     if (n < 0)
     {
-        return 0;
+        return 0; // Returning 0 to total count
     }
 
+    // Combination possible
     if (n == 0)
     {
-        return 1;
+        return 1; // Returning 1 to total count
     }
 
     int cnt = 0;
@@ -22,8 +26,29 @@ int cntCombs(int n)
     {
         cnt += cntCombs(n - i);
     }
+    return cnt % mod;
+}
+*/
 
-    return cnt;
+// DP
+ll cntCombs(int n)
+{
+    ll cnt = 0;
+    vector<ll> comb(n + 1, 0);
+
+    comb[0] = 1; // When n = 0 => There is only one combination that we do not throw any dice
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= 6; j++)
+        {
+            if (i - j >= 0)
+            {
+                comb[i] += (comb[i - j]) % mod;
+            }
+        }
+    }
+
+    return comb[n] % mod;
 }
 
 void solve()
