33c46d7db03b97cc1caa6272abe4139c69fbbc49
utkarsh-009 <utkarshpatil321@gmail.com>
2022-09-21 15:59:45 +0530

    Done

--- Dice_Combinations.cpp
+++ Dice_Combinations.cpp
@@ -2,19 +2,34 @@
 #define ll long long
 #define mod 1000000007
 using namespace std;
- 
-void solve()
+
+int cntComb(int n)
 {
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    if (n < 0)
+    {
+        return -1;
+    }
+
     
 }
- 
+
+void solve()
+{
+    int n;
+    cin >> n;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
- 
-    solve();
 
+    solve();
 }
 
 /*
