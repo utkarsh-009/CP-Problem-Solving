a160290a44cb9995c0a9eb300c4c7c9358e34be8
utkarsh-009 <utkarshpatil321@gmail.com>
2022-10-03 14:50:14 +0530

    Done

--- B_Tea_with_Tangerines.cpp
+++ B_Tea_with_Tangerines.cpp
@@ -9,44 +9,19 @@ void solve()
     cin >> n;
 
     vector<int> a(n);
-    int curr_min = INT_MAX, curr_max = INT_MIN;
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
-        curr_min = min(curr_min, a[i]);
     }
 
-    int cnt = 0;
+    int x = 2 * a[0] - 1;
+    ll cnt = 0;
     for (int i = 0; i < n; i++)
     {
-        if (2 * curr_min > a[i])
+        cnt += a[i] / x;
+        if (a[i] % x == 0)
         {
-            curr_max = max(curr_max, a[i]);
-        }
-        else
-        {
-            if (a[i] / curr_max >= 2)
-            {
-                int mf = a[i] / curr_max;
-                int rem = a[i] - (mf * curr_max);
-                int newVal = curr_max + rem / mf;
-
-                curr_max = max(curr_max, newVal);
-                curr_min = min(curr_min, newVal);
-
-                cnt += mf;
-            }
-            else
-            {
-                int mf = a[i] / curr_min;
-                int rem = a[i] - (mf * curr_min);
-                int newVal = curr_min + rem / mf;
-
-                curr_max = max(curr_max, newVal);
-                curr_min = min(curr_min, newVal);
-
-                cnt++;
-            }
+            cnt--;
         }
     }
 
