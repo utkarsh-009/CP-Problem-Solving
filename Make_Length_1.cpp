ac4f4cf86ac8d5bd1cd7ef67b6d47b4a3699925f
utkarsh-009 <utkarshpatil321@gmail.com>
2022-11-05 21:54:41 +0530

    Done

--- Make_Length_1.cpp
+++ Make_Length_1.cpp
@@ -3,6 +3,24 @@
 #define mod 1000000007
 using namespace std;
 
+/*
+Q:
+You can perform the following operation on SS:
+OP1:	Select an index i (1≤i<∣S∣) such that Si=Si+1.
+OP2:	Set Si to 0 and remove Si+1 from the string.
+
+M1: Using Stack
+-initially, pushing first character of string into stack
+- if top of stack equal to current char of string
+- if match not found simply push current char of string
+- If length already became 1 =>  The string can be made into length 1
+  else => To make length = 1 possible, there should be only '0' or only '1' left.
+
+M2:
+If any of the continuous block of 1's has odd length,
+it’s not possible to remove it completely and so the answer is “No”.
+*/
+
 void solve()
 {
     int n;
@@ -12,29 +30,34 @@ void solve()
     cin >> s;
 
     stack<char> st;
-    st.push(s[0]);
+    st.push(s[0]); // initially, pushing first character of string
 
     for (int i = 1; i < n; i++)
     {
-        if (st.top() == s[i])
+        if (st.top() == s[i]) //  if top of stack equal to current char of string
         {
+            // Performing operation 2
             st.pop();
             st.push('0');
         }
-        else
+        else // if match not found simply push current char of string
         {
             st.push(s[i]);
         }
     }
+
+    // If length already became 1 => The string can be made into length 1
     if (st.size() == 1)
     {
         cout << "YES" << endl;
     }
+    // if length not 1, then we will check the matches created after removing the above matches
     else
     {
         char c = st.top();
         st.pop();
 
+        // To make length = 1 possible, there should be only '0' or only '1' left.
         while (!st.empty() && st.top() == c)
         {
             st.pop();
