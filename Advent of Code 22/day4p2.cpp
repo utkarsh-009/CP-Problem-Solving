546d6119fe424d79dac7149af2acd993eae61b34
utkarsh-009 <utkarshpatil321@gmail.com>
2022-12-04 11:22:58 +0530

    Done

---
+++ Advent of Code 22/day4p2.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#include <sstream>
+#define ll long long
+#define mod 1000000007
+using namespace std;
+
+void solve()
+{
+#ifndef ONLINE_JUDGE
+    freopen("day4INPUT.txt", "r", stdin);
+    // freopen("output.txt", "w", stdin);
+#endif
+
+    int t;
+    cin >> t;
+
+    int ans = 0;
+    for (int q = 0; q < t; q++)
+    {
+        string s;
+        cin >> s;
+
+        int l1, r1, l2, r2;
+
+        string num = "";
+
+        int i = 0;
+        while (s[i] != '-')
+        {
+            num += s[i];
+            i++;
+        }
+        istringstream(num) >> l1;
+        // cout << num << endl;
+
+        num = "";
+        i++;
+        while (s[i] != ',')
+        {
+            num += s[i];
+            i++;
+        }
+        istringstream(num) >> r1;
+        // cout << num << endl;
+
+        num = "";
+        i++;
+        while (s[i] != '-')
+        {
+            num += s[i];
+            i++;
+        }
+        istringstream(num) >> l2;
+
+        // cout << num << endl;
+        num = "";
+        i++;
+        while (i != s.length())
+        {
+            num += s[i];
+            i++;
+        }
+        // cout << num << endl;
+        istringstream(num) >> r2;
+        if (l1 <= l2 && l2 <= r1 || l1 <= r2 && r2 <= r1 || l2 <= l1 && l1 <= r2 || l2 <= r1 && r1 <= r2)
+        {
+            ans++;
+        }
+    }
+
+    cout << ans;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+}
+
+/*
+ASCII VALUES
+    0-9 => 48-57
+    A-Z => 65-90
+    a-z => 97-122
+*/
--- Advent of Code 22/test.cpp
+++ Advent of Code 22/test.cpp
@@ -7,48 +7,9 @@ using namespace std;
 
 int main()
 {
-    ll sum = 0, maxSum = 0, num;
-    string s;
 
-    vector<int> v;
-    ifstream filein("day1INPUT.txt");
-
-    if (filein.is_open())
-    {
-        while (!filein.fail())
-        {
-            string line;
-
-            // filein >> num;
-            getline(filein, line);
-
-            stringstream strToint(line);
-            int num = 0;
-            strToint >> num;
-
-            if (!filein.fail() && line.empty())
-            {
-                maxSum = max(maxSum, sum);
-                v.push_back(sum);
-                sum = 0;
-                continue;
-            }
-
-            if (!filein.fail())
-            {
-                sum += num;
-            }
-        }
-    }
-    else
-    {
-        cout << "File Cannot Open" << endl;
-    }
-
-    sort(v.begin(), v.end(), greater<int>());
-    cout << v[0] + v[1] + v[2] << endl;
-    // cout << v[0] << endl;
-    // cout << v[1] << endl;
-    // cout << v[2] << endl;
-    // cout << maxSum;
+    string num = "28";
+    int i;
+    istringstream(num) >> i;
+    cout << i;
 }
