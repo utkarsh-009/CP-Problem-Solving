0decd17b19ab5900c755eebeb29d46ce9aba9ef0
utkarsh-009 <utkarshpatil321@gmail.com>
2023-01-21 22:35:00 +0530

    To Do

--- C_Quiz_Master.cpp
+++ C_Quiz_Master.cpp
@@ -12,20 +12,6 @@ void solve()
 
     int n, m;
     cin >> n >> m;
-
-    set<int> s;
-
-    for (int i = 0; i < n; i++)
-    {
-        int x;
-        cin >> x;
-        s.insert(x);
-    }
-
-    for (int i = m; i >= 2; i--)
-    {
-        
-    }
 }
 
 int main()
