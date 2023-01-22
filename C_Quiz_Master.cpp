f5cf1d0bccd68dca3c0d8b130a832a252e8af4ce
utkarsh-009 <utkarshpatil321@gmail.com>
2023-01-22 11:43:56 +0530

    Done

--- C_Quiz_Master.cpp
+++ C_Quiz_Master.cpp
@@ -12,6 +12,77 @@ void solve()
 
     int n, m;
     cin >> n >> m;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+    vector<vector<int>> primeFact(n);
+
+    // storing prime factors of each of the numbers: stored as -> [idx] -> pf's of student value
+    for (int i = 0; i < n; i++) // index of student
+    {
+        int stud = a[i];
+        for (int j = 1; j * j <= stud && j <= m; j++)
+        {
+            if (stud % j == 0)
+            {
+                primeFact[i].push_back(j);
+                if (j != stud / j && ((stud / j) <= m))
+                {
+                    primeFact[i].push_back(stud / j);
+                }
+            }
+        }
+    }
+
+    // counts vector stores no of student proficient in particular topic i
+    vector<int> counts(m + 1, 0);
+    // no of topics having atleast one student having proficieny
+    int profTopics = 0;
+    int ans = INT_MAX;
+
+    // USING SLIDING WINDOW TECHNIQUE
+    for (int j = 0, i = 0; j < n; j++)
+    {
+        // updating counts and profTopics variable
+        for (auto &f : primeFact[j])
+        {
+            counts[f]++;
+            if (counts[f] == 1)
+            {
+                profTopics++;
+            }
+        }
+
+        // if team forms for atleast one student proficient for every topic
+        while (profTopics == m)
+        {
+            ans = min(ans, a[j] - a[i]);
+            for (auto &f : primeFact[i])
+            {
+                counts[f]--;
+                if (counts[f] == 0)
+                {
+                    profTopics--;
+                }
+            }
+
+            i++;
+        }
+    }
+
+    if (ans == INT_MAX)
+    {
+        cout << -1;
+    }
+    else
+    {
+        cout << ans;
+    }
 }
 
 int main()
