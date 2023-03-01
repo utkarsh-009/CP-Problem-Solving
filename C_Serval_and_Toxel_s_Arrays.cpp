a70236e016e932bbab6e9eb41019487a71413d80
utkarsh-009 <utkarshpatil321@gmail.com>
2023-03-01 22:24:43 +0530

    To Do

--- C_Serval_and_Toxel_s_Arrays.cpp
+++ C_Serval_and_Toxel_s_Arrays.cpp
@@ -5,61 +5,7 @@ using namespace std;
 
 void solve()
 {
-    // #ifndef ONLINE_JUDGE
-    //     freopen("input.txt", "r", stdin);
-    //     freopen("output.txt", "w", stdin);
-    // #endif
-
-    int n, m;
-    cin >> n >> m;
-
-    map<ll, ll> mp;
-    vector<ll> arr(n);
-    for (int i = 0; i < n; i++)
-    {
-        int x;
-        cin >> x;
-
-        mp[x] = m + 1;
-        arr[i] = x;
-    }
-
-    for (int i = 0; i < m; i++)
-    {
-        int p, v;
-        cin >> p >> v;
-        p -= 1;
-
-        if (mp.find(arr[p]) != mp.end())
-        {
-            mp[arr[p]] = i - m;
-        }
-        else
-        {
-            mp[arr[p]] = 0;
-        }
-
-        arr[p] = v;
-
-        if (mp.find(arr[p]) != mp.end())
-        {
-            mp[arr[p]] = m - i;
-        }
-        else
-        {
-            mp[arr[p]] = 0;
-        }
-    }
-
-    ll ans = m * (m + 1) * n;
-    for (auto e : mp)
-    {
-        ll x = e.second;
-        ll s = (x * (x - 1)) / 2;
-        ans -= s;
-    }
-
-    cout << ans;
+    
 }
 
 int main()
